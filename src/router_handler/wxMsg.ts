37de8dad77ade1b533b5d7bfb16d7de03fe53a1c
wjq <1092229612@qq.com>
2023-06-19 10:50:08 +0800

    feat:添加微信消息解密

--- src/router_handler/wxMsg.ts
+++ src/router_handler/wxMsg.ts
@@ -1,4 +1,23 @@
+import crypto from 'crypto'
+
 export const getWxMsg = (req: any, res: any) => {
   console.log(req)
   console.log(req.query)
+  if (req.query.timestamp) {
+    const { signature, echostr, timestamp, nonce } = req.query
+    let a = crypto.createHash('sha1').update([process.env.wxToken, timestamp, nonce].sort().join('')).digest('hex')
+    if (a == signature) {
+      res.send(echostr)
+    } else {
+      res.send({
+        status: 500,
+        message: '非法!',
+      })
+    }
+  } else {
+    res.send({
+      status: 500,
+      message: '非法!',
+    })
+  }
 }
