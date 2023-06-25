4aae8b6a8dc255a6389f0e33068c315d7577ce69
wjq <1092229612@qq.com>
2023-06-25 13:49:45 +0800

    feat:添加微信消息回执

--- src/router/wxMsg.ts
+++ src/router/wxMsg.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getWxMsg } from '../router_handler/wxMsg'
+import { getWxMsg, postWxMsg } from '../router_handler/wxMsg'
 
 const wxMsgRouter = express.Router()
 
 wxMsgRouter.get('/wxMsg/getWxMsg', getWxMsg)
+wxMsgRouter.post('/wxMsg/postWxMsg', postWxMsg)
 
 export default wxMsgRouter
--- src/router_handler/wxMsg.ts
+++ src/router_handler/wxMsg.ts
@@ -1,23 +1,21 @@
 import crypto from 'crypto'
+import dotenv from 'dotenv'
 
 export const getWxMsg = (req: any, res: any) => {
-  console.log(req)
-  console.log(req.query)
-  if (req.query.timestamp) {
-    const { signature, echostr, timestamp, nonce } = req.query
-    let a = crypto.createHash('sha1').update([process.env.wxToken, timestamp, nonce].sort().join('')).digest('hex')
-    if (a == signature) {
-      res.send(echostr)
-    } else {
-      res.send({
-        status: 500,
-        message: '非法!',
-      })
-    }
+  dotenv.config({ path: __dirname + '/../../.env' })
+  const { signature = '', echostr = '', timestamp = '', nonce = '' } = req.query
+  let sha1 = crypto.createHash('sha1').update([process.env.wxToken, timestamp, nonce].sort().join('')).digest('hex')
+  if (sha1 == signature) {
+    res.send(echostr)
   } else {
     res.send({
       status: 500,
       message: '非法!',
     })
   }
+}
+
+export const postWxMsg = (req: any, res: any) => {
+  console.log(req.body)
+  res.send('')
 }
