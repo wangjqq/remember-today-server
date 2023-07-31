28bcf7d471c4758391d3dbbe1297920b9566ee13
WJQ <1092229612@qq.com>
2023-07-31 14:28:03 +0800

    feat:系统管理新增查询和添加更新信息接口

--- src/router/system.ts
+++ src/router/system.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getSystemInfo } from '../router_handler/system'
+import { getSystemInfo, getUpdateInfo, addUpdateInfo } from '../router_handler/system'
 
 const systemRouter = express.Router()
 
 systemRouter.get('/system/getSystemInfo', getSystemInfo)
+systemRouter.get('/system/getUpdateInfo', getUpdateInfo)
+systemRouter.post('/system/addUpdateInfo', addUpdateInfo)
 
 export default systemRouter
--- src/router_handler/system.ts
+++ src/router_handler/system.ts
@@ -15,3 +15,33 @@ export const getSystemInfo = (req: any, res: any) => {
     })
   })
 }
+
+export const getUpdateInfo = (req: any, res: any) => {
+  const sqlStr = 'SELECT * FROM update_info'
+
+  db.query(sqlStr, {}, (err, results) => {
+    if (err) {
+      return res.cc(err)
+    }
+    res.send({
+      code: '200',
+      message: 'success',
+      data: results,
+    })
+  })
+}
+
+export const addUpdateInfo = (req: any, res: any) => {
+  const sqlStr = 'INSERT INTO update_info SET ?'
+
+  db.query(sqlStr, req.body, (err, results) => {
+    if (err) {
+      return res.cc(err)
+    }
+    res.send({
+      code: '200',
+      message: 'success',
+      data: results,
+    })
+  })
+}
